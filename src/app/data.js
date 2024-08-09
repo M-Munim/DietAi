89ed58586490bb183aaef420abb870e77b6d93e7
munim <muhammadmunimoff330@gmail.com>
2024-08-09 15:16:23 +0500

    questions addede

--- src/app/data.js
+++ src/app/data.js
@@ -12,142 +12,142 @@ export const quiz = {
       "type": "multiple-choice",
       "options": ["Under 18", "18-28", "29-39", "40-50", "51-61", "61 or older"]
     },
-    // {
-    //   "id": 3,
-    //   "question": "What is your gender?",
-    //   "type": "multiple-choice",
-    //   "options": ["Male", "Female", "Non-binary", "Prefer not to say", "Other"]
-    // },
-    // {
-    //   "id": 4,
-    //   "question": "What is your current weight?",
-    //   "type": "multiple-choice",
-    //   "options": ["Underweight", "Normal weight", "Overweight", "Obese"]
-    // },
-    // {
-    //   "id": 5,
-    //   "question": "What is your height?",
-    //   "type": "multiple-choice",
-    //   "options": ["Below 5'0", "5'0 - 5'5", "5'6 - 6'0", "Above 6'0"]
-    // },
-    // {
-    //   "id": 6,
-    //   "question": "What is your primary health goal?",
-    //   "type": "multiple-choice",
-    //   "options": ["Weight loss", "Muscle gain", "Maintenance", "Improved energy"]
-    // },
-    // {
-    //   "id": 7,
-    //   "question": "Do you have any dietary restrictions or allergies?",
-    //   "placeholder": 'Specify any dietary restrictions or allergies',
-    //   "type": "text"
-    // },
-    // {
-    //   "id": 8,
-    //   "question": "How would you describe your activity level?",
-    //   "type": "multiple-choice",
-    //   "options": ["Sedentary", "Lightly active", "Moderately active", "Very active"]
-    // },
-    // {
-    //   "id": 9,
-    //   "question": "How many days per week do you engage in regular physical activity, and what type of exercise do you do?",
-    //   "type": "multiple-choice",
-    //   "options": ["0 days", "1-2 days (please specify)", "3-4 days (please specify)", "5 or more days (please specify)"]
-    // },
-    // {
-    //   "id": 10,
-    //   "question": "How many hours of sleep do you typically get per night?",
-    //   "type": "multiple-choice",
-    //   "options": ["Less than 6 hours", "6-8 hours", "More than 8 hours"]
-    // },
-    // {
-    //   "id": 11,
-    //   "question": "Do you have any specific dietary preferences or dislikes?",
-    //   "type": "multiple-choice",
-    //   "options": ["Vegetarian", "Vegan", "Low-carb", "Other"]
-    // },
-    // {
-    //   "id": 12,
-    //   "question": "What is your typical daily water intake?",
-    //   "type": "multiple-choice",
-    //   "options": ["Less than 1 liter", "1-2 liters", "2-3 liters", "More than 3 liters"]
-    // },
-    // {
-    //   "id": 13,
-    //   "question": "Are there any specific foods or food groups you want to include or avoid in your diet plan?",
-    //   "type": "multiple-choice",
-    //   "options": ["Include specific foods", "Avoid specific foods", "No specific preferences"]
-    // },
-    // {
-    //   "id": 14,
-    //   "question": "How many meals do you prefer to eat in a day?",
-    //   "type": "multiple-choice",
-    //   "options": ["3 meals", "4-5 meals (including snacks)", "6 or more small meals"]
-    // },
-    // {
-    //   "id": 15,
-    //   "question": "Do you have any specific meal timing preferences?",
-    //   "type": "multiple-choice",
-    //   "options": ["Regular meal schedule", "Intermittent fasting", "Other"]
-    // },
-    // {
-    //   "id": 16,
-    //   "question": "What is your usual breakfast like?",
-    //   "type": "multiple-choice",
-    //   "options": ["High protein", "High fiber", "Balanced mix of nutrients", "Other"]
-    // },
-    // {
-    //   "id": 17,
-    //   "question": "What is your usual lunch like?",
-    //   "type": "multiple-choice",
-    //   "options": ["Lean protein and vegetables", "Whole grains and legumes", "Balanced mix of nutrients", "Other"]
-    // },
-    // {
-    //   "id": 18,
-    //   "question": "Do you snack between meals? If yes, what are your go-to snacks?",
-    //   "type": "multiple-choice",
-    //   "options": ["Fruits", "Nuts and seeds", "Yogurt", "Other"]
-    // },
-    // {
-    //   "id": 19,
-    //   "question": "Are you currently taking any dietary supplements?",
-    //   "type": "multiple-choice",
-    //   "options": ["Yes (please specify)", "No"]
-    // },
-    // {
-    //   "id": 20,
-    //   "question": "How comfortable are you with cooking, and how much time are you willing to spend on meal preparation each day?",
-    //   "type": "multiple-choice",
-    //   "options": ["Very comfortable, willing to spend time", "Somewhat comfortable, moderate time", "Not comfortable, prefer quick and easy meals"]
-    // },
-    // {
-    //   "id": 21,
-    //   "question": "Do you have any medical conditions or concerns that I should be aware of when creating your diet plan?",
-    //   "type": "multiple-choice",
-    //   "options": ["Yes (please specify)", "No"]
-    // },
-    // {
-    //   "id": 22,
-    //   "question": "How do you handle stress, and are there any stress triggers that affect your eating habits?",
-    //   "type": "multiple-choice",
-    //   "options": ["Stress eater", "Lose appetite under stress", "No significant impact"]
-    // },
-    // {
-    //   "id": 23,
-    //   "question": "Are there any social or cultural factors that may impact your dietary choices?",
-    //   "type": "multiple-choice",
-    //   "options": ["Yes (please specify)", "No"]
-    // },
-    // {
-    //   "id": 24,
-    //   "question": "How often do you dine out or eat food prepared outside of your home?",
-    //   "type": "multiple-choice",
-    //   "options": ["Rarely", "Occasionally", "Frequently"]
-    // }, {
-    //   "id": 25,
-    //   "question": "Is there anything else you think is important for me to know when creating your personalized diet plan?",
-    //   "placeholder": 'Select one: Yes, No',
-    //   "type": "text"
-    // },
+    {
+      "id": 3,
+      "question": "What is your gender?",
+      "type": "multiple-choice",
+      "options": ["Male", "Female", "Non-binary", "Prefer not to say", "Other"]
+    },
+    {
+      "id": 4,
+      "question": "What is your current weight?",
+      "type": "multiple-choice",
+      "options": ["Underweight", "Normal weight", "Overweight", "Obese"]
+    },
+    {
+      "id": 5,
+      "question": "What is your height?",
+      "type": "multiple-choice",
+      "options": ["Below 5'0", "5'0 - 5'5", "5'6 - 6'0", "Above 6'0"]
+    },
+    {
+      "id": 6,
+      "question": "What is your primary health goal?",
+      "type": "multiple-choice",
+      "options": ["Weight loss", "Muscle gain", "Maintenance", "Improved energy"]
+    },
+    {
+      "id": 7,
+      "question": "Do you have any dietary restrictions or allergies?",
+      "placeholder": 'Specify any dietary restrictions or allergies',
+      "type": "text"
+    },
+    {
+      "id": 8,
+      "question": "How would you describe your activity level?",
+      "type": "multiple-choice",
+      "options": ["Sedentary", "Lightly active", "Moderately active", "Very active"]
+    },
+    {
+      "id": 9,
+      "question": "How many days per week do you engage in regular physical activity, and what type of exercise do you do?",
+      "type": "multiple-choice",
+      "options": ["0 days", "1-2 days (please specify)", "3-4 days (please specify)", "5 or more days (please specify)"]
+    },
+    {
+      "id": 10,
+      "question": "How many hours of sleep do you typically get per night?",
+      "type": "multiple-choice",
+      "options": ["Less than 6 hours", "6-8 hours", "More than 8 hours"]
+    },
+    {
+      "id": 11,
+      "question": "Do you have any specific dietary preferences or dislikes?",
+      "type": "multiple-choice",
+      "options": ["Vegetarian", "Vegan", "Low-carb", "Other"]
+    },
+    {
+      "id": 12,
+      "question": "What is your typical daily water intake?",
+      "type": "multiple-choice",
+      "options": ["Less than 1 liter", "1-2 liters", "2-3 liters", "More than 3 liters"]
+    },
+    {
+      "id": 13,
+      "question": "Are there any specific foods or food groups you want to include or avoid in your diet plan?",
+      "type": "multiple-choice",
+      "options": ["Include specific foods", "Avoid specific foods", "No specific preferences"]
+    },
+    {
+      "id": 14,
+      "question": "How many meals do you prefer to eat in a day?",
+      "type": "multiple-choice",
+      "options": ["3 meals", "4-5 meals (including snacks)", "6 or more small meals"]
+    },
+    {
+      "id": 15,
+      "question": "Do you have any specific meal timing preferences?",
+      "type": "multiple-choice",
+      "options": ["Regular meal schedule", "Intermittent fasting", "Other"]
+    },
+    {
+      "id": 16,
+      "question": "What is your usual breakfast like?",
+      "type": "multiple-choice",
+      "options": ["High protein", "High fiber", "Balanced mix of nutrients", "Other"]
+    },
+    {
+      "id": 17,
+      "question": "What is your usual lunch like?",
+      "type": "multiple-choice",
+      "options": ["Lean protein and vegetables", "Whole grains and legumes", "Balanced mix of nutrients", "Other"]
+    },
+    {
+      "id": 18,
+      "question": "Do you snack between meals? If yes, what are your go-to snacks?",
+      "type": "multiple-choice",
+      "options": ["Fruits", "Nuts and seeds", "Yogurt", "Other"]
+    },
+    {
+      "id": 19,
+      "question": "Are you currently taking any dietary supplements?",
+      "type": "multiple-choice",
+      "options": ["Yes (please specify)", "No"]
+    },
+    {
+      "id": 20,
+      "question": "How comfortable are you with cooking, and how much time are you willing to spend on meal preparation each day?",
+      "type": "multiple-choice",
+      "options": ["Very comfortable, willing to spend time", "Somewhat comfortable, moderate time", "Not comfortable, prefer quick and easy meals"]
+    },
+    {
+      "id": 21,
+      "question": "Do you have any medical conditions or concerns that I should be aware of when creating your diet plan?",
+      "type": "multiple-choice",
+      "options": ["Yes (please specify)", "No"]
+    },
+    {
+      "id": 22,
+      "question": "How do you handle stress, and are there any stress triggers that affect your eating habits?",
+      "type": "multiple-choice",
+      "options": ["Stress eater", "Lose appetite under stress", "No significant impact"]
+    },
+    {
+      "id": 23,
+      "question": "Are there any social or cultural factors that may impact your dietary choices?",
+      "type": "multiple-choice",
+      "options": ["Yes (please specify)", "No"]
+    },
+    {
+      "id": 24,
+      "question": "How often do you dine out or eat food prepared outside of your home?",
+      "type": "multiple-choice",
+      "options": ["Rarely", "Occasionally", "Frequently"]
+    }, {
+      "id": 25,
+      "question": "Is there anything else you think is important for me to know when creating your personalized diet plan?",
+      "placeholder": 'Select one: Yes, No',
+      "type": "text"
+    },
   ]
 };
