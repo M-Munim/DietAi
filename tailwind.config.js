e6a2c582e1e8b497e101e02989e4495580185298
munim <muhammadmunimoff330@gmail.com>
2024-08-15 11:11:49 +0500

    nav, footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // content: [
+  //   // diet
+  //   // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //   // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  //   // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+  //   // './pages/**/*.{js,ts,jsx,tsx}',
+  //   // './components/**/*.{js,ts,jsx,tsx}',
+  //   // './layouts/**/*.{js,ts,jsx,tsx}',
+  //   // next ui
+  //   // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+
+  // ],
+
   content: [
-    // diet
-    // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // './pages/**/*.{js,ts,jsx,tsx}',
-    // './components/**/*.{js,ts,jsx,tsx}',
-    // './layouts/**/*.{js,ts,jsx,tsx}',
-    // next ui
-    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-
-
-
-
-
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
