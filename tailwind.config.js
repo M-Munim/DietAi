f789d61781a89dd9446c2b9b0e28cc86f635a48d
munim <muhammadmunimoff330@gmail.com>
2024-08-15 11:05:49 +0500

    nav, footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,24 @@
 module.exports = {
   content: [
     // diet
+    // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // './pages/**/*.{js,ts,jsx,tsx}',
+    // './components/**/*.{js,ts,jsx,tsx}',
+    // './layouts/**/*.{js,ts,jsx,tsx}',
+    // next ui
+    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+
+
+
+
+
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-    // next ui
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
